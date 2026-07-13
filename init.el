7cc6cd26618ffb300339f0764a46efebcc6990df
Carson <jackparadise520a@gmail.com>
2026-07-13 19:34:20 +0800

    💪 能與系統剪貼簿溝通

--- init.el
+++ init.el
@@ -32,7 +32,32 @@
   (evil-mode 1))   ; 開啟Evil Mode. 很多Emacs套件都是 (xxx-mode 1)
 ;; 等同: `package-install evil`
 
+
+
+(defun my/pbcopy (text &optional _push)
+  "Copy TEXT to the macOS clipboard."
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" nil "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(defun my/pbpaste ()
+  "Return text from the macOS clipboard."
+  (string-trim-right
+   (shell-command-to-string "pbpaste")))
+
+(setq interprogram-cut-function   #'my/pbcopy)
+(setq interprogram-paste-function #'my/pbpaste)
+
 ;;; 🟧 可選項設定
+;; t 指的是 true ;
+;; nil或0 預設行為;
+;; 負數(通常-1): 關閉
+
+(setq select-enable-clipboard t) ; 與系統剪貼簿同步.
+;; (setq select-enable-primary t) ; 在 Linux/X11 中，通常有兩種「剪貼簿」： 1. PRIMARY (只要選取文字，就會自動放進，不需要Ctrl+C)  2. CLIPBOARD.  Note: macOS沒有Primary Selection的概念，所以加了也是沒用
+
+
 ;; ;; 不顯示工具列
 ;; (tool-bar-mode -1)
 ;;
