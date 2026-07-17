4e75c40bb7ebba7ca1756cf00508a88210a76122
Carson <jackparadise520a@gmail.com>
2026-07-17 13:54:31 +0800

    <leader>cd, :Cd 切換工作目錄到當前檔案的資料夾

--- lisp/config/keymaps.el
+++ lisp/config/keymaps.el
@@ -1,3 +1,11 @@
+(defun cd-here ()
+  "切換工作目錄到當前檔案的資料夾"
+  (interactive)
+  (unless buffer-file-name
+    (user-error "Current buffer is not visiting a file"))
+  (cd (file-name-directory buffer-file-name))
+  (message "cwd => %s" default-directory))
+
 (with-eval-after-load 'evil
   ;; nnoremap <A-w> <C-w>w<CR>
   ;; nnoremap <A-h> <C-w>h<CR>
@@ -9,6 +17,9 @@
   (define-key evil-normal-state-map (kbd "M-j") #'windmove-down)
   (define-key evil-normal-state-map (kbd "M-k") #'windmove-up)
   (define-key evil-normal-state-map (kbd "M-l") #'windmove-right)
+
+  (evil-ex-define-cmd "Cd"                             #'cd-here)
+  (define-key evil-normal-state-map (kbd "<leader>cd") #'cd-here)
   )
 
 (provide 'config/keymaps)
